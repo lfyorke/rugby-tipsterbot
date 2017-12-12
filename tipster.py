000ff46c21f9cbfb3ba54b0ea272d76f9fc1d9de
lfyorke <Leo.Yorke@keyrus.com>
2017-12-12 16:27:29 +0000

    Added timing decorator

--- tipster.py
+++ tipster.py
@@ -1,10 +1,24 @@
 from sklearn.neural_network import MLPClassifier
-import pandas as pd 
+import pandas as pd
+from functools import wraps
+from time import time
 
 
-def transform(data)
-"""Aggregate the data to prepare the required features for analysis"""
-pass
+def timeit(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print('func: {0} args: [{1}, {2}] took: {3} sec'.format(f.__name__, args, kw, te-ts))
+        return result
+    return wrap
+
+@timeit
+def transform():
+    """Aggregate the data to prepare the required features for analysis"""
+
+    pass
 
 
 if __name__ == "__main__":
